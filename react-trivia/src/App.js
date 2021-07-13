4afd97fbd9183c5793b5f92567e5f130b35eb2b3
bhall20 <brianhall401@yahoo.com>
2021-07-12 20:05:19 -0400

    Recieving TypeError: categories.map is not a function error

---
+++ react-trivia/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App () {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple')
+      .then(res => setCategories(res.data))
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="App">
+        <h1>Trivia</h1>
+      </div>
+      <div>
+        <h1>Categories</h1>
+        {categories.map(category => {
+          return (
+            <div key={category.slug}>
+              <p>{category.Category}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App;
