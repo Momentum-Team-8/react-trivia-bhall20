a4e71994e545b327aceb9aa4ea7249a50bf3fde8
bhall20 <brianhall401@yahoo.com>
2021-07-14 11:50:33 -0400

    Added category list to new components folder

--- react-trivia/src/App.js
+++ react-trivia/src/App.js
@@ -1,32 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { CategoryList } from './components/CategoryList';
 
-function App () {
-  const [categories, setCategories] = useState([])
+function App() {
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    axios.get('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple')
-      .then(res => setCategories(res.data))
-  }, [])
+    axios
+      .get("https://opentdb.com/api_category.php")
+      .then((res) => setCategories(res.data.trivia_categories));
+  }, []);
 
   return (
-    <div className="container">
-      <div className="App">
-        <h1>Trivia</h1>
-      </div>
-      <div>
-        <h1>Categories</h1>
-        {categories.map(category => {
-          return (
-            <div key={category.slug}>
-              <p>{category.Category}</p>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <CategoryList categories={ categories } />
   )
 }
-
 export default App;
