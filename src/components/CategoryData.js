3a3318416571f22c4178379c6288926aebd720c8
bhall20 <brianhall401@yahoo.com>
2021-07-21 09:39:10 -0400

    added styling

--- src/components/CategoryData.js
+++ src/components/CategoryData.js
@@ -6,12 +6,8 @@ import { AnswerChoices } from './AnswerChoices'
 export const CategoryData = (props) => {
   const [categoryData, setCategoryData] = useState({})
   const [loading, setLoading] = useState(true)
-  const [answered, setAnswered] = useState(false)
-  const [correct, setCorrect] = useState(false)
-  const { selectedCategory } = props
-  const refreshPage = () => {
-    window.location.reload()
-  }
+  const [score, setScore] = useState(0)
+  const { selectedCategory, setSelectedCategory } = props
 
   useEffect(() => {
     getCategoryData(selectedCategory).then(data => {
@@ -19,13 +15,6 @@ export const CategoryData = (props) => {
       setLoading(false)
     })
   }, [selectedCategory])
-  const commitAnswer = () => {
-    if (correct) {
-      console.log('You answered ', correct)
-      console.log(answered)
-    }
-    setAnswered(false)
-  }
 
   return loading
     ? 'Category data is loading'
@@ -33,8 +22,9 @@ export const CategoryData = (props) => {
       <>
         <div className='category-header'>
           <p className='title hero is-info'>{selectedCategory.name}</p>
-          <button className='button is-primary' onClick={refreshPage}>Back to Category List</button>
+          <button className='button is-primary' onClick={() => setSelectedCategory(null)}>Back to Category List</button>
         </div>
+        <h1>{score}</h1>
         <div className='questions'>
           {categoryData.map((data) => {
             return (
@@ -42,10 +32,9 @@ export const CategoryData = (props) => {
                 <p><strong>{he.decode(data.question)}</strong></p>
                 <section key={data.question}>
                   <AnswerChoices
-                    answers={{ correctAnswer: he.escape(data.correct_answer), incorrectAnswers: data.incorrect_answers }}
-                    checkAnswer={setCorrect}
-                    setAnswered={setAnswered}
-                    commitAnswer={commitAnswer}
+                    answers={{ correctAnswer: he.decode(data.correct_answer), incorrectAnswers: data.incorrect_answers }}
+                    setScore={setScore}
+                    score={score}
                   />
                 </section>
               </div>
